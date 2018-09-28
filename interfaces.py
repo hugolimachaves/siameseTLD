c789276353f831523d1593d00132e7637b52c44c
Hugo <hugo.lima.chaves@gmail.com>
2018-09-28 17:15:59 -0300

    algum progresso, porem ainda temos bug - esqueleto da integracao quase completa

--- interfaces.py
+++ interfaces.py
@@ -7,7 +7,7 @@ shared_library = CDLL('TLD/bin/Debug/libTLD.so')
 
 SIZE_ARRAY = 32
 LAST_ADDED = -1
-SIZE_DESCRIPTOR = 255
+SIZE_DESCRIPTOR = 256
 
 bad_windows  = []
 good_windows = []
@@ -37,7 +37,7 @@ feature_neg_obj_model = []
 #   - 5: (Centro_X, Centro_Y, Width, Height, Frame_Number)
 
 def convertSimilatiry(siameseDistance):
-	return 1 / (siameseDistance+1) # retorna a distancia no TLD
+	return 1 / (siameseDistance + 1) # retorna a distancia no TLD
 	
 def getLength(element): # verifica o tamanho total de elementos em uma estrutura de dados de dimensoes arbitrarias
 	if isinstance(element, list):
@@ -50,7 +50,7 @@ def getDescriptor(bb):
 	#for _ in range(SIZE_DESCRIPTOR):
 	
 	descriptor = np.random.randn(1,SIZE_DESCRIPTOR)
-	#print('descriptor'.center(100,'#'),descriptor)
+
 	return descriptor
 
 # passa  as deep Features dos candidatos para o presente frame conjuntamente
@@ -59,66 +59,86 @@ def distCandidatesToTheModel(deep_features_candidates, isPositive=True):
 	#Usa os seguintes parametros globais:  feature_pos_obj_model, feature_neg_obj_model  
 	features = []
 
+
 	if isPositive: # modelo positivo do object model
 		positiveLabel = [1 for i in feature_pos_obj_model]
 		labels = np.asarray(positiveLabel)
-		#mod 
-
 		features = np.asarray(feature_pos_obj_model)
-		#~mod
 	
 	else: # modelo negativo do object model
 		negativeLabel = [0 for i in feature_neg_obj_model] 
 		labels = np.asarray(negativeLabel)
-		#mod
 		features = np.asarray(feature_neg_obj_model)
-	
-	print('feature_neg_obj_model: ', feature_neg_obj_model)
-	print('feature_pos_obj_model: ', feature_pos_obj_model)
-	
-	print(features)
-	knn_1 = KNeighborsClassifier(n_neighbors=1)
-
-	#list_features = []
-	#list_features.append(features)
-	#list_features = deep_features_candidates
-	print('features len printando:',len(features))
-	print('Dimensao de features: ', features.shape ,' Dimensao de label', labels.shape)
-	knn_1.fit(features, labels)
 	
 	distances = []
 	positions = []
-	for candidate in deep_features_candidates: # pega a menor distancia para cada candidato na lista deep_features_candidate
-		list_candidate = []
-		list_candidate.append(candidate)
-		dist,position = knn_1.kneighbors(list_candidate, n_neighbors=1, return_distance=True)
-		distances.append(dist)
-		positions.append(position)
-		# example: neigh.kneighbors([[1., 1., 1.]])
-		# pode das errado porque a documentacao mostra um array de array
 
-	return distances, positions # retorna a menor distancia em relacao ao modelo, eh uma lista pois sao varios candidatos e tambem  a posicao no vetor
+	print('features.size ', features.size)
+	if (features.size is not 0):
+		knn_1 = KNeighborsClassifier(n_neighbors=1)
+
+		if(False):
+			print('features len printando:',len(features[LAST_ADDED]))
+			print('Dimensao de features: ', features.shape ,' Dimensao de label', labels.shape)
+			
+			print("labels".center(70,'*'))
+			print(labels)
+
+			print("features".center(70,'*'))
+			print(features)
+			#print('type of features: ', type(features), ' shape of features: ', features.reshape((features.shape[-1]) ))
+	
+		listFeatures = [bb for frame in features for bb in frame]
+		knn_1.fit(listFeatures, labels)
+		
+		for candidate in deep_features_candidates: # pega a menor distancia para cada candidato na lista deep_features_candidate
+			list_candidate = np.asarray(candidate)
+			dist,position = knn_1.kneighbors(list_candidate, n_neighbors=1, return_distance=True)
+			distances.append(dist[0][0])
+			positions.append(position)
+			# example: neigh.kneighbors([[1., 1., 1.]])
+			# pode das errado porque a documentacao mostra um array de array
+
+	return distances # retorna a menor distancia em relacao ao modelo, eh uma lista pois sao varios candidatos e tambem  a posicao no vetor
+
+	#return distances # retorna a menor distancia em relacao ao modelo, eh uma lista pois sao varios candidatos e tambem  a posicao no vetor
 
 # passo uma lista de bb dos candidatos e uma bb do tracker (lista de lista)
 def detSimilarity(candidates, bb_tracker, is_neg_empty=False, is_pos_empty=False, is_candidates_empty=False, is_bb_tracker_empty=False):
+	# candidates:
 	# sera utilizado - (sao globais): positive_obj_model, negative_obj_model
+
+	positive_distances_candidates = []
+	negative_distances_candidates = []
+	positive_distances_tracker = []
+	negative_distances_tracker = []
+
 	if(not is_neg_empty):
-		for bb in negative_obj_model[LAST_ADDED]:
+		bb_list_model = [BB for frame in negative_obj_model for BB in frame]
+		feature_aux = []
+		for bb in bb_list_model:
 			descriptor = getDescriptor(bb)
 			feature_neg_obj_model.append(descriptor) # o tamanho e equivalente ao numero de descritores negativos
-
-		assert(getLength(feature_neg_obj_model) == getLength(negative_obj_model)), 'o tamanho do object Model difere do tamanho dos descritores'
+			feature_aux.append(descriptor)
+		print('len feat ', len(feature_aux),'len neg mod ',len(negative_obj_model[LAST_ADDED]))
+		assert(len(feature_aux) == len(negative_obj_model[LAST_ADDED])), 'o tamanho do object Model difere do tamanho dos descritores'
 
 	if(not is_pos_empty):
-		for bb in positive_obj_model[LAST_ADDED]:
+		bb_list_model = [BB for frame in positive_obj_model for BB in frame]
+		feature_aux = []
+		for bb in bb_list_model:	
 			descriptor = getDescriptor(bb)
 			feature_pos_obj_model.append(descriptor)  # o tamanho e equivalente ao numero de descritores positivos
-
-		assert(getLength(feature_pos_obj_model) == getLength(positive_obj_model)), 'o tamanho do object Model difere do tamanho dos descritores'
+			feature_aux.append(descriptor)
+		if(False):
+			print('feature_pos_obj_model: ', feature_pos_obj_model, 'tamanho: ',feature_pos_obj_model[0].shape)
+			print('\npositive_obj_model: ', positive_obj_model)
+		print('len feat ', len(feature_aux),'len pos mod ',len(positive_obj_model))
+		assert(len(feature_aux) == len(positive_obj_model)), 'o tamanho do object Model difere do tamanho dos descritores'
 
 	if(not is_candidates_empty):
 		deep_features_candidates = []
-		for bb in candidates:
+		for bb in candidates[LAST_ADDED]:
 			#TODO fazer o descritor
 			descriptor = getDescriptor(bb)
 			'''
@@ -129,22 +149,58 @@ def detSimilarity(candidates, bb_tracker, is_neg_empty=False, is_pos_empty=False
 			deep_features_candidates.append(descriptor)
 		deep_features_candidates = np.asarray(deep_features_candidates)
 		#print('Info deep_features_candidates: ',  deep_features_candidates, '\n tipo: ', type(deep_features_candidates) )
+
 		positive_distances_candidates = distCandidatesToTheModel(deep_features_candidates, isPositive=True)
 		negative_distances_candidates = distCandidatesToTheModel(deep_features_candidates, isPositive=False)
 
 	if(not is_bb_tracker_empty):
 		feature_tracker = []
-		for bb in bb_tracker:
+		for bb in bb_tracker[LAST_ADDED]:
 			descriptor = getDescriptor(bb)
 			feature_tracker.append(descriptor)
 		feature_tracker =  np.asarray(feature_tracker)
+
 		positive_distances_tracker = distCandidatesToTheModel(feature_tracker, isPositive=True)
 		negative_distances_tracker = distCandidatesToTheModel(feature_tracker, isPositive=False)
 
-	return 2 #positive_distances_candidates, negative_distances_candidates,
-			 #positive_distances_tracker,	negative_distances_tracker
 
+	print('positive_distances_candidates: ',positive_distances_candidates)
+	print('negative_distances_candidates: ',negative_distances_candidates)
+	print('positive_distances_tracker: ',positive_distances_tracker)
+	print('negative_distances_tracker: ',negative_distances_tracker)
 
+	positive_siam_sim_cand = [convertSimilatiry(distancia) for distancia in positive_distances_candidates]
+	negative_siam_sim_cand = [convertSimilatiry(distancia) for distancia in negative_distances_candidates]
+	positive_siam_sim_bb_tracker = [convertSimilatiry(distancia) for distancia in positive_distances_tracker]
+	negative_siam_sim_bb_tracker = [convertSimilatiry(distancia) for distancia in negative_distances_tracker]
+
+	print('positive_siam_sim_cand: ',positive_siam_sim_cand)
+	print('negative_siam_sim_cand: ',negative_siam_sim_cand)
+	print('positive_siam_sim_bb_tracker: ',positive_siam_sim_bb_tracker)
+	print('negative_siam_sim_bb_tracker: ',negative_siam_sim_bb_tracker)
+
+	return positive_siam_sim_cand, negative_siam_sim_cand, positive_siam_sim_bb_tracker, negative_siam_sim_bb_tracker
+
+def read_data(array, array_size, frame):
+	bb_list = []
+	is_empty = True
+	if(array_size is not 0):
+		bb_pos = []
+		for i in range(array_size):
+			bb_pos.append(array[i])
+
+			if(i%4==0 and i is not 0):
+				bb_pos.append(frame)
+				bb_list.append(bb_pos)
+				bb_pos = []
+
+		bb_pos.append(frame)
+		bb_list.append(bb_pos)
+		bb_pos = []
+
+		is_empty = False
+
+	return bb_list, is_empty
 
 #'frame' se refere ao numero do frame que esta sendo processado no codigo .py
 def init_interface(frame=2): 
@@ -164,7 +220,7 @@ def init_interface(frame=2):
 	'''
 
 	HULL_SIZE = 4
-	WINDOW_SIZE = 40 # Example
+	WINDOW_SIZE = 40 # Examplebb_list
 	OBJECT_MODEL_SIZE = 400 # Example
 	#print("Caminho atual e:",)
 	parameters_path = os.getcwd() + "/dataset/exemplo/01-Light_video00001/parameters.yml"
@@ -197,84 +253,23 @@ def init_interface(frame=2):
 	print('\nFrame de entrada: '+ str(frame)+ ' Frame de retorno: ' + str(retorno_frame.value) + '\n')
 	assert (frame == retorno_frame.value), "Conflito nos frames"
 
-	is_neg_empty        = True
-	is_pos_empty        = True
-	is_good_empty       = True
-	is_good_hull_empty  = True
-
-
-
-
-
-	print('Size Positive: ' + str(size_positive.value))
-	if(size_positive.value is not 0):
-		print(' Os valores do object model ++ '.center(70,'*'))
-		bb_list = []
-		bb_pos = []
-		for i in range(size_positive.value):
-			bb_pos.append(array_object_model_positive[i])
-
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				print()
-				
-			print(str(array_object_model_positive[i])+' ',end='')
-
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
-		positive_obj_model.append(bb_list)
-		is_pos_empty = False
-
-	print('\n\n\nSize Negative: ' + str(size_negative.value))
-	if(size_negative.value is not 0):
-		print(' Os valores do object model -- '.center(70,'*'))
-		bb_list = []
-		bb_pos = []
-		for i in range(size_negative.value):
-			bb_pos.append(array_object_model_negative[i])
-
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				print()
-				
-			print(str(array_object_model_negative[i])+' ',end='')
-
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
+	bb_list, is_neg_empty = read_data(array_object_model_negative, size_negative.value, frame)
+	if(not is_neg_empty):
 		negative_obj_model.append(bb_list)
-		is_neg_empty = False
 
-	print('\n\n')
-	if(size_good_windows.value is not 0):
-		print(' Os valores do good windows '.center(70,'*'))
-		bb_list = []
-		bb_pos = []
-		for i in range(size_good_windows.value):
-			bb_pos.append(array_good_windows[i])
+	bb_list, is_pos_empty = read_data(array_object_model_positive, size_positive.value, frame)
+	if(not is_pos_empty):
+		positive_obj_model.append(bb_list)
 
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				print()
-				
-			print(str(array_good_windows[i])+' ',end='')
-
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
+	bb_list, is_good_empty = read_data(array_good_windows, size_good_windows.value, frame)
+	if(not is_good_empty):
 		good_windows.append(bb_list)
-		is_good_empty = False
 
+	bb_list, is_good_hull_empty = read_data(array_good_windows_hull, size_good_windows_hull.value, frame)
+	if(not is_good_hull_empty):
+		good_windows.append(bb_list)
+
+	'''
 	print('\n\n')
 	if(size_good_windows_hull.value is not 0):
 		print(' Os valores do good windows hull '.center(70,'*'))
@@ -288,6 +283,7 @@ def init_interface(frame=2):
 
 		good_windows_hull.append(bb_pos)
 		is_good_hull_empty = False
+	'''
 
 def TLD(frame):
 	
@@ -322,82 +318,26 @@ def TLD(frame):
 	candidates = []
 	bb_tracker = []
 
-	is_neg_empty  = True
-	is_pos_empty  = True
 	is_candidates_empty = True
 	is_bb_tracker_empty = True
 
-	print('\n')
-	print('Size Positive: ' + str(size_positive.value))
-	if(size_positive.value is not 0):
-		print(' Os valores do object model ++ '.center(70,'*'))
-		bb_list = [] # contem todas as listas de bb
-		bb_pos = []
-		for i in range(size_positive.value):
-			bb_pos.append(array_object_model_positive[i])
-
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				
-			print(str(array_object_model_positive[i])+' ',end='') # BB para cada frame
-
-		# sao executadas N vezes o iterador i. A ultima vez nao e executada no loop e sim logo abaixo
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
-		positive_obj_model.append(bb_list) # todos o BB dos object Model postivo ate o momento
-		is_pos_empty = False
-
-	print('\n\n')
-	print('Size Negative: ' + str(size_negative.value))
-	if(size_negative.value is not 0):
-		print(' Os valores do object model -- '.center(70,'*'))
-		bb_list = []
-		bb_pos = []
-		for i in range(size_negative.value):
-			bb_pos.append(array_object_model_negative[i])
-
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				print()
-				
-			print(str(array_object_model_negative[i])+' ',end='')
-		# sao executadas N vezes o iterador i. A ultima vez nao e executada no loop e sim logo abaixo
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
+	bb_list, is_neg_empty = read_data(array_object_model_negative, size_negative.value, frame)
+	if(not is_neg_empty):
 		negative_obj_model.append(bb_list)
-		is_neg_empty = False
 
-	if(size_candidates.value is not 0):
-		print('\n\n')
-		print(' Os valores das bb candidates '.center(70,'*'))
-		bb_list = []
-		bb_pos = []
-		for i in range(size_candidates.value):
-			bb_pos.append(array_bb_candidates[i])
+	bb_list, is_pos_empty = read_data(array_object_model_positive, size_positive.value, frame)
+	if(not is_pos_empty):
+		positive_obj_model.append(bb_list)
 
-			if(i%4==0 and i is not 0):
-				bb_pos.append(frame)
-				bb_list.append(bb_pos)
-				bb_pos = []
-				print()
-				
-			print(str(array_bb_candidates[i])+' ',end='')
-		# sao executadas N vezes o iterador i. A ultima vez nao e executada no loop e sim logo abaixo
-		bb_pos.append(frame)
-		bb_list.append(bb_pos)
-		bb_pos = []
-
+	bb_list, is_candidates_empty = read_data(array_bb_candidates, size_candidates.value, frame)
+	if(not is_candidates_empty):
 		candidates.append(bb_list)
-		is_candidates_empty = False
 
+	bb_list, is_bb_tracker_empty = read_data(array_bb_tracker, size_bb_tracker.value, frame)
+	if(not is_bb_tracker_empty):
+		bb_tracker.append(bb_list)
+
+	'''
 	# BB de referencia do tracker
 	if(size_bb_tracker.value is not 0):
 		print('\n\n')
@@ -412,8 +352,8 @@ def TLD(frame):
 
 		bb_tracker.append(bb_pos)
 		is_bb_tracker_empty = False
-
 	print('\n\n')
+	'''
 
 	# candidates[ N ][ 5 ]
 	#   - N: Numero de bb no frame
@@ -423,16 +363,52 @@ def TLD(frame):
 	#   - 1: Para ser considerado uma lista no algoritmos
 	#   - 5: (Centro_X, Centro_Y, Width, Height, Frame_Number)
 
+	
+	positive_distances_candidates = []
+	negative_distances_candidates = []
+	positive_distances_tracker = []
+	negative_distances_tracker = []
+
+
+
 	# passa uma lista de bb dos candidatos retornado pelo TLD e passa uma bb retornado pelo Tracker no TLD
+	positive_distances_candidates, negative_distances_candidates, positive_distances_tracker, negative_distances_tracker = detSimilarity(candidates, bb_tracker, is_neg_empty, is_pos_empty, is_candidates_empty, is_bb_tracker_empty)
 
-	retorno = detSimilarity(candidates, bb_tracker, is_neg_empty, is_pos_empty, is_candidates_empty, is_bb_tracker_empty)
+	print('positive_distances_candidates'.center(70,'*'),positive_distances_candidates)
+	print('negative_distances_candidates'.center(70,'*'),negative_distances_candidates)
+	print('positive_distances_tracker'.center(70,'*'),positive_distances_tracker)
+	print('negative_distances_tracker'.center(70,'*'),negative_distances_tracker)
+	
+	retorno_frame = c_int()
 
-	shared_library.TLD_function_2(byref(retorno_frame))
+	size_good_windows      = c_int(0) # tamanho do vetor array good windows
+	size_good_windows_hull = c_int(0) # tamanho do vetor array good_windows_hull (que e sempre 4)
 
-	assert (frame == retorno_frame.value), "Conflito nos frames"
+	similaridade_positiva_candidates = [-1] * ARRAY_SIZE
+	similaridade_negativa_candidates = [-1] * ARRAY_SIZE
+	similaridade_positiva_bb_tracker = [-1] * ARRAY_SIZE
+	similaridade_negativa_bb_tracker = [-1] * ARRAY_SIZE
+
+	array_good_windows      = [-1] * ARRAY_SIZE
+	array_good_windows_hull = [-1] * ARRAY_SIZE
+
+	similaridade_positiva_candidates = (c_float * ARRAY_SIZE) (*similaridade_positiva_candidates)
+	similaridade_negativa_candidates = (c_float * ARRAY_SIZE) (*similaridade_negativa_candidates)
+	similaridade_positiva_bb_tracker = (c_float * ARRAY_SIZE) (*similaridade_positiva_bb_tracker)
+	similaridade_negativa_bb_tracker = (c_float * ARRAY_SIZE) (*similaridade_negativa_bb_tracker)
+
+	array_good_windows      = (c_float * ARRAY_SIZE) (*array_good_windows)
+	array_good_windows_hull = (c_float * ARRAY_SIZE) (*array_good_windows_hull)
+
+	shared_library.TLD_function_2(similaridade_positiva_candidates, similaridade_negativa_candidates,
+								  similaridade_positiva_bb_tracker, similaridade_negativa_bb_tracker,
+								  array_good_windows, byref(size_good_windows),
+								  array_good_windows_hull, byref(array_good_windows_hull))
 
 init_interface(1)
-TLD(2)
+
+for i in range(2,7):
+	TLD(i)
  
 #'deepDescriptor' eh o descritor que sera passado para o codigo C. eh um descritor de 128/256 floats.
 def interface2(deepDescriptor,frame):
